c3b9f269fd21ec38f2d1114bb6e90d1eaa31099b
Pablo <pabrodez@gmail.com>
2020-12-30 01:37:09 +0000

    Add rollup fix module support

--- rollup.config.js
+++ rollup.config.js
@@ -1,44 +1,19 @@
 import typescript from '@rollup/plugin-typescript';
 import pkg from './package.json';
 
-const input = './src/index.ts'
+export default {
+    input: './src/index.ts',
 
-export default [
-    {
-        input: input,
-        output: {
+    output: [
+        {
             format: 'esm',
             file: pkg.module,
-            sourcemap: false,
         },
-        plugins: [
-            typescript()
-        ]
-    },
-    {
-        input: input,
-        output: {
-            format: 'cjs',
-            file: pkg.main,
-            sourcemap: false,
-            exports: 'default'
-        },
-        plugins: [
-            typescript()
-        ]
-    },
-    {
-        input: input,
-        output: {
+        {
             format: 'umd',
-            name: 'SgfParser',
-            file: pkg.unpkg,
-            sourcemap: false,
-        },
-        plugins: [
-            // terser(),
-            typescript()
-        ]
-    },
-
-]
+            file: pkg.main,
+            name: 'SgfParser'
+        }
+    ],
+    plugins: [typescript()]
+}
